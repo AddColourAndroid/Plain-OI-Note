e217cba1f36625fdba30283c092575b870b9dbc6
Mpho Nkuna <mcnkuna@addcolour.co.za>
2018-10-25 11:10:34 +0200

    Added a RecyclerView to the main activity

--- app/src/main/java/za/co/addcolour/plainoinote/MainActivity.java
+++ app/src/main/java/za/co/addcolour/plainoinote/MainActivity.java
@@ -1,31 +1,43 @@
 package za.co.addcolour.plainoinote;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.LinearLayoutManager;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import za.co.addcolour.plainoinote.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding mBinding;
+
+    private LinearLayoutManager mManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        initialize();
+        setSupportActionBar(mBinding.toolbar);
+        mBinding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    private void initialize() {
+
+        mManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, true);
+        mBinding.contentMain.recyclerView.setLayoutManager(mManager);
+
     }
 
     @Override
